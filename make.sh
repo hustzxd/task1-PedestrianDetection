13d3e78330c4b4eab8d2b1bf0ca7d2e336afbfd5
zhaoxiandong <2227820226@qq.com>
2018-04-21 16:32:05 +0800

    add utils used in yolo :rain:

--- make.sh
+++ make.sh
@@ -1,6 +1,14 @@
-cd models/layers/reorg/src
-echo "Compiling reorg layer kernels by nvcc..."
-nvcc -c -o reorg_cuda_kernel.cu.o reorg_cuda_kernel.cu -x cu -Xcompiler -fPIC -arch=sm_52
-cd ../
-# python build.py
+#!/usr/bin/env bash
+
+CUDA_PATH=/usr/local/cuda/
+
+cd models/utils
+python build.py build_ext --inplace
 cd ../../
+
+# cd models/layers/reorg/src
+# echo "Compiling reorg layer kernels by nvcc..."
+# nvcc -c -o reorg_cuda_kernel.cu.o reorg_cuda_kernel.cu -x cu -Xcompiler -fPIC -arch=sm_52
+# cd ../
+# python build.py
+# cd ../../
